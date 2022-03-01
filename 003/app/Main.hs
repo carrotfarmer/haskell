1e8f017445fc3055ca612a40efaeb4586fd66444
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-03-01 12:43:39 +0530

    fix: formatted

--- 003/app/Main.hs
+++ 003/app/Main.hs
@@ -2,21 +2,21 @@ data List a = Empty | Cons a (List a)
 
 toHList :: List a -> [a]
 toHList Empty = []
-toHList (Cons x xs) = x : toHList(xs)
+toHList (Cons x xs) = x : toHList (xs)
 
 map' f [] = []
-map' f (x:xs) = f x : map' f xs
+map' f (x : xs) = f x : map' f xs
 
-data Error a = Error | Ok a deriving Show
+data Error a = Error | Ok a deriving (Show)
 
-safeDivide :: Error Int -> Error Int -> Error Int 
-safeDivide (Error) _ = Error 
-safeDivide _ (Error) = Error 
-safeDivide (Ok a) (Ok 0) = Error 
-safeDivide (Ok a) (Ok b) = Ok (a `div` b) 
+safeDivide :: Error Int -> Error Int -> Error Int
+safeDivide (Error) _ = Error
+safeDivide _ (Error) = Error
+safeDivide (Ok a) (Ok 0) = Error
+safeDivide (Ok a) (Ok b) = Ok (a `div` b)
 
-data Person = Person {
-  name :: String,
-  id :: Int,
-  dob :: (Int, Int, Int)
-}
+data Person = Person
+  { name :: String,
+    id :: Int,
+    dob :: (Int, Int, Int)
+  }

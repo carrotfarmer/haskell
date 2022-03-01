babe85e9770f405278dbee9bd50aa77f44705f95
Dhruva Srinivas <dhruvas17068@gmail.com>
2022-03-01 12:34:51 +0530

    feat: types

---
+++ 003/app/Main.hs
@@ -0,0 +1,22 @@
+data List a = Empty | Cons a (List a)
+
+toHList :: List a -> [a]
+toHList Empty = []
+toHList (Cons x xs) = x : toHList(xs)
+
+map' f [] = []
+map' f (x:xs) = f x : map' f xs
+
+data Error a = Error | Ok a deriving Show
+
+safeDivide :: Error Int -> Error Int -> Error Int 
+safeDivide (Error) _ = Error 
+safeDivide _ (Error) = Error 
+safeDivide (Ok a) (Ok 0) = Error 
+safeDivide (Ok a) (Ok b) = Ok (a `div` b) 
+
+data Person = Person {
+  name :: String,
+  id :: Int,
+  dob :: (Int, Int, Int)
+}
